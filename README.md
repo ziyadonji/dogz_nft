f177b916dbc889c21d8ff104adeaebbb695c5571
Ziyad c <ziyadonji@gmail.com>
2022-07-03 00:05:14 +0530

    documentation of food token

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dogz_nft
  
+//FoodToken.sol
+
+The contract is deployed in binance testnet smart chain.
+contract address  = 0x2b3CF815aeFE283B335b5823d80F3fFb9004c84E
+Token tracking url = https://testnet.bscscan.com/token/0x2b3CF815aeFE283B335b5823d80F3fFb9004c84E
+
+
+
+
--- foodToken.sol
+++ foodToken.sol
@@ -1,14 +1,24 @@
 // contracts/FoodToken.sol
 // SPDX-License-Identifier: MIT
+//compiler version declaration.
 pragma solidity ^0.8.0;
 
+// import ERC20 contract to use all standard funcitons and libraries
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// extending the implementation of erc20 as foodToken
 contract FoodToken is ERC20 {
+    // setting the initial supply of the token
     int initialSupply=10000000;
+
+    // one time genesis minting of initial supply = 10000000 FOOD token with ticker symbol FOOD
     constructor() ERC20("FOOD", "FOOD") {
+        // minting and transfer of token to sender of the message using standard implementation of _mint
         _mint(msg.sender, initialSupply);
     }
+
+    // overriding the standard decimal 18 with 0 decimals
+    // 0 decimals means token is not divisible
     function decimals() public view virtual override returns (uint8) {
   return 0;
 }
